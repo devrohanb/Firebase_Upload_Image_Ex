f4380012e3760909819fa6db7a5ebc07415011b2
devrohanb <dev.rohanb@gmail.com>
2018-07-29 10:27:10 +0530

    Initial commit

---
+++ app/src/main/java/com/dev/rohanb/firebaseUploadImg/Upload.java
@@ -0,0 +1,45 @@
+package com.dev.rohanb.firebaseUploadImg;
+
+public class Upload
+{
+    private String mName;
+    private String mImageUrl;
+
+    public Upload()
+    {
+        //empty Constructor Needed.
+    }
+
+    public Upload(String name,String imageUrl)
+    {
+        if (name.trim().equals(""))
+        {
+            name = "No Name";
+        }
+        mName=name;
+        mImageUrl=imageUrl;
+    }
+
+
+    public String getName()
+    {
+        return mName;
+    }
+
+    public void setName(String name)
+    {
+        mName=name;
+    }
+
+    public String getImageUrl()
+    {
+        return mImageUrl;
+    }
+
+    public void setImageUrl(String imageUrl)
+    {
+        mImageUrl=imageUrl;
+    }
+
+
+}
